a5e7da945a05fa25ac42f9eb33b848663be5b5d7
apprajapati9 <apprajapati@hotmail.com>
2021-01-27 09:41:49 -0800

    project created, bottom Num/pair UI done

---
+++ app/src/main/java/com/ajay/memorygame/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.ajay.memorygame
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var recyclerViewBoard: RecyclerView
+    private lateinit var textViewNumMoves: TextView
+    private lateinit var textViewNumPairs: TextView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        recyclerViewBoard = findViewById(R.id.rcMoves)
+        textViewNumMoves = findViewById(R.id.tvNumMoves)
+        textViewNumPairs = findViewById(R.id.tvNumPairs)
+    }
+}
